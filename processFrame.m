e51710bec3adb4528075a77ad672f5d715491f1e
umbacht <thomas.umb@gmail.com>
2021-12-12 10:28:02 +0100

    Changed Name

---
+++ processFrame.m
@@ -0,0 +1,45 @@
+function [S_current, T_WC_current] = processFrame(Image_current, Image_previous, S_previous)
+    % 1. Associate keypoints in the current frame to previous landmarks
+    % 2. Estimate current camera pose
+    % 3. Triangulate new landmarks not previously found
+
+
+
+
+
+    %% Find keypoints from pointcloud 3D -> 2D
+    
+    
+    tracker = 0; %0: Harris, 1: KLT
+    
+    %% Harris Tracking
+    
+    if ~tracker
+    
+        
+        
+        % Harris scores
+    
+        % Select Keypoints
+    
+        % Descirbe query Keypoints
+        % Descirbe database Keypoints
+    
+        % match descriptors
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+
+
+end
