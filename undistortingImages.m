1096383a0f40d01a2f27e6cfcd606887a1711a21
umbacht <thomas.umb@gmail.com>
2022-01-02 20:52:58 +0100

    Update undistortingImages.m

--- undistortingImages.m
+++ undistortingImages.m
@@ -18,6 +18,7 @@ alpha_c = 0.000000000000000;
 kc = [ 0.054879289895450 ; -0.106881360666110 ; 0.003970921416368 ; 0.011588508763675 ; 0.000000000000000 ];
 tangential = [kc(3) kc(4)];
 radial = [kc(1) kc(2) kc(5)];
+
 %-- Image size:
 nxny = [4032 3024];
 
@@ -28,31 +29,19 @@ cameraParams = cameraParameters('IntrinsicMatrix',K,'TangentialDistortion',tange
 
 last_frame = 2116;
 
-% Undistort Images: 
-% for i = 1:last_frame
-%     image = imread([walking_path '/images/' sprintf('Image_%d.jpg',i)]);
-%     image = rgb2gray(image);
-%     %imshow(image);
-%     %pause(0.01)
-% 
-%     % Undistort image and safe in dataset
-%     J = undistortImage(image,cameraParams);
-%     imwrite(J,[walking_path '/undistorted_images/' sprintf('Image_%d.jpg',i)]);
-% end
+Undistort Images: 
+for i = 1:last_frame
+    image = imread([walking_path '/images/' sprintf('Image_%d.jpg',i)]);
+    image = rgb2gray(image);
+    %imshow(image);
+    %pause(0.01)
+
+    % Undistort image and safe in dataset
+    J = undistortImage(image,cameraParams);
+    imwrite(J,[walking_path '/undistorted_images/' sprintf('Image_%d.jpg',i)]);
+end
 
 
-% calibration:
-% PROBLEM: Datatype is 3024x4032x4 (last one is 255)
-
-% images = imageDatastore(fullfile([walking_path '/calibration_images/']));
-% %images = imageDatastore(fullfile(toolboxdir('vision'),'visiondata','calibration','mono'));
-% 
-% [imagePoints, boardSize] = detectCheckerboardPoints(images.Files);
-% squareSize = 27; %ours: 27
-% worldPoints = generateCheckerboardPoints(boardSize,squareSize);
-% I = readimage(images,1);
-% imageSize = [size(I,1),size(I,2)];
-% cameraParams = estimateCameraParameters(imagePoints, worldPoints, 'ImageSize', imageSize)
 
 
 
